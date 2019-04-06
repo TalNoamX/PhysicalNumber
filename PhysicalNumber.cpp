02c6a614c891bb32ffea9cbc90b66120ddf07ff8
amitai <amitaymizrahy@gmail.com>
2019-04-06 22:24:42 +0300

    build the first version of the PhysicalNumber class

---
+++ PhysicalNumber.cpp
@@ -0,0 +1,61 @@
+#include "PhysicalNumber.hpp"
+
+using namespace std;
+using ariel::Unit;
+using ariel::PhysicalNumber;
+
+ariel::PhysicalNumber::PhysicalNumber(double val, Unit cat)
+{
+	this->value = val;
+	this->category = cat;
+}
+PhysicalNumber &operator + (PhysicalNumber &b)
+{
+	return b;
+}
+PhysicalNumber &operator += (PhysicalNumber &b)
+{
+	return b;
+}
+PhysicalNumber &operator - (PhysicalNumber &b)
+{
+	return b;
+}
+PhysicalNumber &operator -= (PhysicalNumber &b)
+{
+	return b;
+}
+PhysicalNumber &operator = (PhysicalNumber &b)
+{
+	return b;
+}
+bool &operator >= (PhysicalNumber &b)
+{
+	return true;
+}
+bool &operator > (PhysicalNumber &b)
+{
+	return true;
+}
+bool &operator <= (PhysicalNumber &b)
+{
+	return true;
+}
+bool &operator < (PhysicalNumber &b)
+{
+	return true;
+}
+bool &operator == (PhysicalNumber &b)
+{
+	return true;
+}
+PhysicalNumber &operator ++ ()
+{
+	return this;
+}
+PhysicalNumber &operator -- ()
+{
+	return this;
+}
+friend istream &operator << (istream &input, PhysicalNumber &phy);
+friend ostream &operator <<(ostream &out, PhysicalNumber &phy);
